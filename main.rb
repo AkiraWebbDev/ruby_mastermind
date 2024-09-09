c4bc2a70151de7178c6814d294307d7676929c3e
Luke Webb <lukewebb2000@gmail.com>
2024-09-09 15:44:53 +1200

    Main now prints welcome message and instantiates the game.

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
+# frozen_string_literal: true
 
+require_relative 'lib/game_loop'
+
+puts '####################################'
+puts '####  Welcome to MASTERMIND!!!  ####'
+puts '####################################'
+
+GameLoop.new
