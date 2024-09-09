207b41b81618a61d960f74c429562dc8d5321102
Luke Webb <lukewebb2000@gmail.com>
2024-09-09 16:51:47 +1200

    Added evaluation logic

--- lib/maker.rb
+++ lib/maker.rb
@@ -7,16 +7,37 @@ class CodeMaker
   attr_reader :code
 
   def initialize(random = false) # rubocop:disable Style/OptionalBooleanParameter
-    @colors = %w[B O G P]
-    if random == true
-      @code = MastermindHelper.manual_code
-    else
+    @colors = %w[B O G P R]
+    if random
       @code = []
       @code.push(@colors.sample) while @code.length < 4
+    else
+      @code = MastermindHelper.manual_code
     end
   end
 
   def show_code
     MastermindHelper.show_code(@code)
+  end
+
+  # Evaluates the guess, returning a string that shows the evaluation of the guess
+  def evaluate(guess)
+    w_pegs = 0
+    r_pegs = 0
+    guess = guess.split
+    code_clone = @code.clone
+    guess.each_with_index do |color, index|
+      next unless code_clone.include?(color)
+
+      code_clone.delete_at(code_clone.index(color))
+      w_pegs += 1
+      if @code[index] == color
+        r_pegs += 1
+        w_pegs -= 1
+      end
+    end
+    return 'WIN' if r_pegs == 4
+
+    "#{w_pegs} W : #{r_pegs} R"
   end
 end
