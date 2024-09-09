1900d1e64e7d61d33bfdb93521f4872a34d7a3c8
Luke Webb <lukewebb2000@gmail.com>
2024-09-09 16:51:01 +1200

    Fix Past guesses logic, removed attr_accessor

--- lib/breaker.rb
+++ lib/breaker.rb
@@ -4,8 +4,6 @@ require_relative 'helper'
 
 # Class that represents the player breaking the code
 class CodeBreaker
-  attr_accessor :past_guesses
-
   def initialize
     @past_guesses = []
   end
@@ -16,6 +14,12 @@ class CodeBreaker
   end
 
   def display_past_guesses
+    puts "\n\nPast guesses:\n"
     @past_guesses.each { |guess| MastermindHelper.show_code(guess) }
+    puts "\n"
+  end
+
+  def add_guess_result(evaluated_guess)
+    @past_guesses.push(evaluated_guess)
   end
 end
